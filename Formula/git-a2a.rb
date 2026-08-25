29006161ecd8b6b8a556400d5a8c6593706da3cd
git-a2a-release <neprel@users.noreply.github.com>
2026-08-25 09:18:55 +0000

    Brew formula update for git-a2a v1.7.0

--- Formula/git-a2a.rb
+++ Formula/git-a2a.rb
@@ -1,16 +1,16 @@
 class GitA2a < Formula
   desc "Import git modules together with their owning agents"
   homepage "https://github.com/neprel/git-a2a"
-  version "1.6.0"
+  version "1.7.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/neprel/git-a2a/releases/download/v1.6.0/git-a2a_brew_1.6.0_darwin_arm64.tar.gz"
-      sha256 "d634f8ebc2cd67447b10de2524b8d887650cbb71a7bf02854f28d4ff9e8db56e"
+      url "https://github.com/neprel/git-a2a/releases/download/v1.7.0/git-a2a_brew_1.7.0_darwin_arm64.tar.gz"
+      sha256 "d0ea50eb6f52c191baccd3b914059a0e6f1ef98f80a39d0463870c1fd6d871bb"
     else
-      url "https://github.com/neprel/git-a2a/releases/download/v1.6.0/git-a2a_brew_1.6.0_darwin_amd64.tar.gz"
-      sha256 "aaba5093f37bb048ead6cc6a51440a13c3c18f0494913340816913a23ba4886b"
+      url "https://github.com/neprel/git-a2a/releases/download/v1.7.0/git-a2a_brew_1.7.0_darwin_amd64.tar.gz"
+      sha256 "66124029520db6e6b1c96562a67d0d25592349a3ab30f98b5854404219853ecb"
     end
   end
 
@@ -20,6 +20,6 @@ class GitA2a < Formula
   end
 
   test do
-    assert_match "git-a2a 1.6.0", shell_output("#{bin}/git-a2a --version")
+    assert_match "git-a2a 1.7.0", shell_output("#{bin}/git-a2a --version")
   end
 end
